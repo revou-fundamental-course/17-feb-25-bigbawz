9f86a9f329b8dbea6e2b244ac83816b5d868fe0d
bigbawz <alvinfulbert.business@gmail.com>
2025-02-27 23:01:19 +0700

    First commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 // This is a JavaScript file
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider() {
+
+    if (slides.length > 0) {
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 3000);
+    }
+
+}
+
+function showSlide(index) {
+
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide() {
+
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
